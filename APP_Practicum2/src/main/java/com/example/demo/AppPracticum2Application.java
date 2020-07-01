49115122bd9c04f4f5d3578cc16ac6fafe422d9c
Bryan <bryan.maldonado.armijos@gmail.com>
2020-07-01 11:07:25 -0500

    Comit inicial con eclipse

---
+++ APP_Practicum2/src/main/java/com/example/demo/AppPracticum2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPracticum2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPracticum2Application.class, args);
+	}
+
+}
